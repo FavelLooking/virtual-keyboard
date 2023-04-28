a526abb629901f1136b2620442f933db996f623d
FavelLooking <intwestpol@gmail.com>
2023-04-29 00:40:09 +0400

    feat: implement virtual keyboard input for tab, del and enter keys

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ let mainContainer = document.createElement("div");
 let keys = ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "+", "Backspace", "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\", "Del", "CapsLock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "Enter", "Shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "▲", "Shift", "Ctrl", "Win", "Alt", " ", "Alt",  "◀", "▼", "▶", "Ctrl"]
 let key;
 let cursorPos;
+let ourSpace = document.getElementById("space")
 
 // Code for moving cursor in textarea
 
@@ -81,22 +82,32 @@ let addSymbolToTextArea = (event) => {
     textArea.value = textArea.value.slice(0, cursorPos-1) + textArea.value.slice(cursorPos);
     textArea.selectionStart = cursorPos - 1;
     textArea.selectionEnd = cursorPos - 1;
+    break;  
+    case "Tab":
+      textArea.value = textArea.value.slice(0, cursorPos) + "    " + textArea.value.slice(cursorPos);
+      textArea.selectionStart = cursorPos + 4;
+      textArea.selectionEnd = cursorPos + 4;
     break;
-  
-    // case "Tab": key.id = "tab";
-    // break;
-    // case "CapsLock": key.id = "caps-lock";
-    // break;
-    // case "Shift": key.id = "shift";
-    // break;
-    // case "Del": key.id = "del";
-    // break;
-    // case "Enter": key.id = "enter";
-    // break;
-    // case " ": key.id = "space";
-    // break;
-    // case "Ctrl": key.id = "ctrl";
-    // break;
+    case "CapsLock":
+    break;
+    case "Shift":
+    break;
+    case "Del": 
+    textArea.value = textArea.value.slice(0, cursorPos) + textArea.value.slice(cursorPos+1);
+    textArea.selectionStart = cursorPos ;
+    textArea.selectionEnd = cursorPos ;
+    break;  
+    case "Enter":
+    textArea.value = textArea.value.slice(0, cursorPos) + "\n" + textArea.value.slice(cursorPos);
+    textArea.selectionStart = cursorPos + 1;
+    textArea.selectionEnd = cursorPos + 1;
+    break;
+    case "Ctrl":
+    break;
+    case "Win":
+    break;
+    case "Alt":
+    break;
     default:
     textArea.value += event.target.innerText
   }
@@ -111,9 +122,22 @@ for (let i = 0; i < allButtons.length; i++) {
 
 textArea.addEventListener("input", ()=> {
   cursorPos = textArea.selectionStart;
-  console.log(cursorPos)
 });
 textArea.addEventListener("click", ()=> {
   cursorPos = textArea.selectionStart;
-  console.log(cursorPos)
+});
+textArea.addEventListener("keydown", (e)=> {
+  cursorPos = textArea.selectionStart;
+  
+  if (e.key === "Tab") {
+    e.preventDefault();
+    textArea.value = textArea.value.slice(0, cursorPos) + "    " + textArea.value.slice(cursorPos);
+    textArea.selectionStart = cursorPos + 4;
+    textArea.selectionEnd = cursorPos + 4;
+  } if(e.key === " ") {
+    
+    textArea.value = textArea.value.slice(0, cursorPos) + " " + textArea.value.slice(cursorPos);
+    textArea.selectionStart = cursorPos+1;
+    textArea.selectionEnd = cursorPos+1;
+  }
 });
